2533155bf4985db9ec22f7836223fa645cc0f08f
glab254-EE <glebvarlam@gmail.com>
2025-09-11 23:50:07 +0500

    KT2.

---
+++ KT2/CatFramework.cs
@@ -0,0 +1,87 @@
+﻿namespace WarlamovGleb_CatFramework
+{
+    public class CatException : ArgumentException
+    {
+        public CatException(string message) : base(message)
+        {
+        }
+    }
+    public abstract class Cat
+    {
+        public abstract int Fluffiness { get; protected set; }
+        public abstract string FluffinessCheck();
+        public override string ToString()
+        {
+            return "A cat with fluffness: " + Fluffiness;
+        }
+    }
+    public class Tiger : Cat
+    {
+        public override int Fluffiness { get; protected set; }
+        public double Weight { get; protected set; }
+
+        public override string FluffinessCheck()
+        {
+            return "Kycb";
+        }
+        public Tiger()
+        {
+            Fluffiness = 50;
+            Weight = 50;
+        }
+        public Tiger(int fluffiness, double weight)
+        {
+            string toThrowNext = "";
+            if (weight > 140 || weight < 0)
+                toThrowNext = ("Unable to create a tiger with weight: " + weight);
+            if (fluffiness < 0 || fluffiness > 100)
+            {
+                if (toThrowNext != "")
+                {
+                    throw new CatException("Unable to create a tiger with fluffiness: " + fluffiness + ".\n" + toThrowNext);
+                }
+                else
+                {
+                    throw new CatException("Unable to create a tiger with fluffiness: " + fluffiness);
+                }
+            }
+            if (toThrowNext != "")
+            {
+                throw new CatException(toThrowNext);
+            }
+            Fluffiness = fluffiness;
+            Weight = weight;
+        }
+        public override string ToString()
+        {
+            return "A tiger with weight: " + Weight + " fluffness: " + Fluffiness;
+        }
+    }
+    public class CuteCat : Cat
+    {
+        public override int Fluffiness { get; protected set; }
+
+        public override string FluffinessCheck()
+        {
+            if (Fluffiness > 75) return "OwO";
+            else if (Fluffiness <= 75 && Fluffiness >= 51) return "Heavy";
+            else if (Fluffiness <= 50 && Fluffiness >= 21) return "Medium";
+            else if (Fluffiness <= 20 && Fluffiness >= 1) return "Slightly";
+            else return "Sphynx";
+        }
+        public override string ToString()
+        {
+            return "A cute cat with fluffiness: " + Fluffiness;
+        }
+        public CuteCat()
+        {
+            Fluffiness = 50;
+        }
+        public CuteCat(int fluffiness)
+        {
+            if (fluffiness < 0 || fluffiness > 140)
+                throw new CatException("Unable to create a cute cat with fluffiness: " + fluffiness);
+            Fluffiness = fluffiness;
+        }
+    }
+}
---
+++ KT2/Program.cs
@@ -0,0 +1,62 @@
+﻿using WarlamovGleb_CatFramework;
+namespace WarlamovGleb_CatApplicationInAerodynamics
+{
+    internal class Program
+    {
+        static void DisplayCatInfo(Cat[] LocalCatsArray, string path)
+        {
+            string toWrite = "";
+            for (int i = 0; i < LocalCatsArray.Length; i++)
+            {
+                toWrite += $"Cat_{i}={{\n\tFluffinessCheck: {LocalCatsArray[i].FluffinessCheck()};\n\tToString: {LocalCatsArray[i]};\n}}\n";
+            }
+            File.WriteAllText(path, toWrite);
+        }
+        static Cat[] GenerateRandomCats(uint count)
+        {
+            Random random = new();
+            Cat[] cats = new Cat[count];
+            int pingpongcounter = 0;
+            int lastCatIndex = 0;
+            do 
+            {
+                try
+                {
+                    if (pingpongcounter == 0)
+                    {
+                        cats[lastCatIndex] = new Tiger(random.Next(-20, 120), random.Next(50, 160));
+                    } else
+                    {
+                        cats[lastCatIndex] = new CuteCat(random.Next(-20, 120));
+                    }
+                    pingpongcounter = pingpongcounter == 1 ? 0 : pingpongcounter+1;
+                    lastCatIndex++;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Exception passed: '"+ex.Message+"'");
+                }
+            } 
+            while (lastCatIndex < count);
+            return cats;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input your desired cats count");
+            uint count = Convert.ToUInt32(Console.ReadLine());
+            Console.Clear();
+            Cat[] cats = GenerateRandomCats(count);
+            Console.WriteLine("Input your information path (WITH .txt ending)");
+            string? path = Console.ReadLine();
+            if (path != null)
+            {
+                DisplayCatInfo(cats, path);
+                Console.WriteLine("Information written to " + path);
+            }
+            else
+            {
+                Console.WriteLine("Path is null");
+            }
+        }
+    }
+}
