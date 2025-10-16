6cd0b7dffd1a9c55c334e5332edba0d41661cfd3
glab254-EE <glebvarlam@gmail.com>
2025-10-16 23:28:35 +0500

    Practise 5 complete

---
+++ PRCTS5/Classes.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal interface IAlghoritm
+    {
+        internal abstract object Alghoritm(object[] args);
+    }
+    internal class Context
+    {
+        private IAlghoritm contextalghoritm;
+        public Context(IAlghoritm alghoritm) => contextalghoritm = alghoritm;
+        internal object ExecuteAlghoritm(object[] args)
+        {
+            return contextalghoritm.Alghoritm(args);
+        }
+    }
+    internal class Add : IAlghoritm
+    {
+        object IAlghoritm.Alghoritm(object[] args)
+        {
+            if (args.Length < 2) throw new ArgumentException("Not enough arguments");
+            double output = 0;
+            output = args.First() is double ? (double)args.First() : 0;
+            args[0] = 0d;
+            foreach (var item in args)
+            {
+                if (item is double)
+                {
+                    output += (double)item;
+                }
+            }
+            return output;
+        }
+    }
+    internal class Substract : IAlghoritm
+    {
+        object IAlghoritm.Alghoritm(object[] args)
+        {
+            if (args.Length < 2) throw new ArgumentException("Not enough arguments");
+            double output = 0;
+            output = args.First() is double ? (double)args.First() : 0;
+            args[0] = 0d;
+            foreach (var item in args)
+            {
+                if (item is double)
+                {
+                    output -= (double)item;
+                }
+            }
+            return output;
+        }
+    }
+    internal class Multiply : IAlghoritm
+    {
+        object IAlghoritm.Alghoritm(object[] args)
+        {
+            if (args.Length < 2) throw new ArgumentException("Not enough arguments");
+            double output = 0;
+            output = args.First() is double ? (double)args.First() : 0;
+            args[0] = 1d;
+            foreach (var item in args)
+            {
+                if (item is double)
+                {
+                    output *= (double)item;
+                }
+            }
+            return output;
+        }
+    }
+}
---
+++ PRCTS5/Program.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleApp2;
+
+Context add = new Context(new Add());
+Context sub = new Context(new Substract());
+Context mul = new Context(new Multiply());
+
+object firstoperation = sub.ExecuteAlghoritm([10d, 5d ]) ;
+double result1 = 0;
+Console.WriteLine(firstoperation.ToString());
+if (firstoperation is double d)
+{
+    result1 = d;
+}
+object secondoperation = add.ExecuteAlghoritm([result1, 25d]);
+double result2 = 0;
+Console.WriteLine(secondoperation.ToString());
+if (secondoperation is double d2)
+{
+    result2 = d2;
+}
+object finaloperation = mul.ExecuteAlghoritm([result2, 2.3d]);
+Console.WriteLine(finaloperation.ToString());
